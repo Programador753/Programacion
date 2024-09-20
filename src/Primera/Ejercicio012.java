8c3fb6ce9aa912409ad9e6ceba7757a0f589863c
Programador753 <antoniohcavero@outlook.com>
2024-09-20 17:28:25 +0200

    clase

--- src/Primera/Ejercicio012.java
+++ src/Primera/Ejercicio012.java
@@ -1,20 +1,16 @@
 /*
 Cálculo de variaciones matemáticas
-variacione de tantos elementos tomadas de tantos en tantos
+Variaciones de tantos elementos tomadas de tantos en tantos
  */
 package Primera;
 
 public class Ejercicio012 {
     public static void main (String arg[]){
-        int elemento = 7;
-        int toma = 4;
-        int acum = 1;
-        for(int cont = 1; cont <= elemento; cont++ )
-        {
-            acum = acum * cont;
-        }
-        
-            
+        int elemento = 7; // Número de elementos 
+        int toma = 4;     // Elementos tomados de tantos en tantos 
+        int acum = 1;     // Variable para acumular el resultado de la variación
+        for (int cont = 0; cont < toma; cont++) 
+            acum *= (elemento - cont);       
+        System.out.println("Las variaciones de " + elemento + " elementos tomados de " + toma + " en " + toma + " son: " + acum);
     }
-    
 }

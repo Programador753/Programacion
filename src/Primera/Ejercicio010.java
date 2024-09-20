c06dba9c76fd5bd6674690cf3276e8a34c4fc666
Programador753 <antoniohcavero@outlook.com>
2024-09-20 15:57:22 +0200

    actualizo

--- src/Primera/Ejercicio010.java
+++ src/Primera/Ejercicio010.java
@@ -7,16 +7,13 @@ public class Ejercicio010 {
     public static void main (String arg[]){
         int numero = 5;
         int acum = 1;
-        int desc = 1;
         for(int cont = 1; cont <= numero; cont++ )
             acum = acum * cont;
         System.out.println("El factorial de " + numero + " es " + acum);
-        System.out.println(numero +"! = " + acum);
-        
+        acum = 1;
         for(int cont = numero; cont >= 1; cont-- )
-            desc = desc * cont;
-        System.out.println("El factorial de " + numero + " es " + desc);
-        System.out.println(numero +"! = " + desc);
+            acum *= cont;
+        System.out.println("El factorial de " + numero + " es " + acum); 
     }
     
 }
---
+++ src/Primera/Ejercicio012.java
@@ -0,0 +1,20 @@
+/*
+Cálculo de variaciones matemáticas
+variacione de tantos elementos tomadas de tantos en tantos
+ */
+package Primera;
+
+public class Ejercicio012 {
+    public static void main (String arg[]){
+        int elemento = 7;
+        int toma = 4;
+        int acum = 1;
+        for(int cont = 1; cont <= elemento; cont++ )
+        {
+            acum = acum * cont;
+        }
+        
+            
+    }
+    
+}

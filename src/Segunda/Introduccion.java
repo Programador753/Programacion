63cdf30f017e6f8f82154256c3c403da092e585f
Programador753 <antoniohcavero@outlook.com>
2024-12-05 10:04:22 +0100

    actualizo

---
+++ src/Segunda/Introduccion.java
@@ -0,0 +1,43 @@
+/*
+Programacion orientada a objetos caracteristicas:
+- Polimorfismo 
+- Herencia
+- Encapsulamiento
+
+En la programacion orientada a objetos nosotos podemos crear nuestras propias clases que tengan su propio metodo ejecutable 
+Atributo, propiedad o caracteristica es lo mismo.
+
+La clase es una definicion pero un objeto es un elemento de esa clase.
+Una clase tiene atributos ejemplo:
+La clase Silla:
+
+class Silla {
+Boolean Respaldo;  // Atributos
+int NumeroPatas;  // Atributos
+
+
+METODOS son los que tratan con los atributos de la clase y se escriben dentro de la clase.
+
+}
+
+Hay un metodo diferente que es el Método Constructor que se usa para construir un objeto de la clase
+y ese metodo se llama igual que su clase y no devuelve nada. 
+El nombre de la clse se escribe con la primera letra en Mayuscula.
+
+Cuando llamemos al metodo constructor lo llamaremos con new y el nombre de la clase.
+El metodo constructor instancia y da valores iniciales a los atributos de la clase.
+
+A las clases publicas puedes acceder desde otro paquete.
+
+Metodos 
+get -> vas a recibir algo 
+
+set -> vas a modificar algo recibiendo un valor 
+
+
+ */
+package Segunda;
+
+public class Introduccion {
+    
+}

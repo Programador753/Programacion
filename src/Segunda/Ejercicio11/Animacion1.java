7f74b5bb20776a1f0249ef665757841cbef6100b
Programador753 <antoniohcavero@outlook.com>
2025-01-22 10:54:11 +0100

    actualizo

--- src/Segunda/Ejercicio11/Animacion1.java
+++ src/Segunda/Ejercicio11/Animacion1.java
@@ -5,10 +5,14 @@ package Segunda.Ejercicio11;
 
 import java.awt.Event; // Importo clase Eventos
 import java.awt.Frame; // Importo clase Frame
+import java.awt.Graphics; // Importo clase Graphics 
 
-public class Animacion1 extends Frame{ 
-    
-    public static void main(String arg[]){
+
+public class Animacion1 extends Frame implements Runnable{ 
+    Thread animacion; // Objeto de la clase Thread
+    String frames[] = {"*", "**", "***", "****", "*****", "******", "*****", "****", "***", "**", "*"}; // Vector de cadenas
+    int actual = 0; // Posicion actual 
+    public static void main(String arg[]){ 
         Animacion1 app = new Animacion1(); // Instancio nuevo objeto de la clase FrameApp    
     }
     
@@ -18,8 +22,14 @@ public class Animacion1 extends Frame{
         this.pack(); // Empaquetar contenido
         this.setSize(200,200); // Tamaño de la ventana
         this.setVisible(true); // Hacerla visible
+        animacion = new Thread(this); // Instanciamos el objeto de la clase Thread
+        animacion.start(); // Start llama al metodo run 
     }
-    
+    public void paint(Graphics g){ // Metodo paint
+        g.drawString(frames[actual], 80, 100); // Dibuja la cadena en la posicion actual
+    }
+
+
     public boolean handleEvent(Event ev) {  // Control de eventos 
         if(ev.id == Event.WINDOW_DESTROY) // Control de evento para cerrar la ventana 
         {
@@ -27,5 +37,16 @@ public class Animacion1 extends Frame{
             return true; // Devuelve verdadero
         }
         return false; // Devuelve falso
+    }
+
+    @Override
+    public void run(){ // Metodo run 
+        do{ // Bucle do while 
+            actual = (actual + 1) % frames.length; // Calcula la posicion actual
+            repaint(); // Llama al metodo repaint
+            try { // Manejo de excepciones 
+                Thread.sleep(300); // Tiempo de espera
+            } catch (InterruptedException ex) {} // Excepcion a manejar 
+        }while(true); // Bucle infinito 
     }
 }

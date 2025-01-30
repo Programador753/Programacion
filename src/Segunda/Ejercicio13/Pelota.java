1f63895e600cdab85823a50d19efeb8330852229
Programador753 <antoniohcavero@outlook.com>
2025-01-30 09:11:44 +0100

    actualizar

--- src/Segunda/Ejercicio13/Pelota.java
+++ src/Segunda/Ejercicio13/Pelota.java
@@ -13,6 +13,7 @@ public class Pelota extends Rectangle{ // Heredamos de la clase Rectangle
     Color color; // Color de la pelota
     Color colores[] = {Color.yellow, Color.blue, Color.lightGray, Color.red, Color.magenta}; // Colores de la pelota
     
+
     public Pelota() {
         super((int)(Math.random()*250), (int)(Math.random()*250), 0, 0); // Creamos un rectangulo con valores aleatorios
         int radio = ((int)(Math.random()*30)) + 20; // Creamos un radio aleatorio
@@ -21,28 +22,21 @@ public class Pelota extends Rectangle{ // Heredamos de la clase Rectangle
         this.velX = (int)(Math.random()*11) - 5; // Asignamos una velocidad aleatoria en X entre -5 y 5
         this.velY = (int)(Math.random()*11) - 5; // Asignamos una velocidad aleatoria en Y entre -5 y 5
         color = colores[(int)(Math.random()*colores.length)]; // Asignamos un color aleatorio
-
-
     }
-    public void paint(Graphics g){  // Metodo paint para dibujar
-        g.setColor(color); // Asignamos el color
-        g.fillOval(x, y, width, height); // Dibujamos la pelota
-        
+    public void paint(Graphics gg){  // Metodo paint para dibujar
+        gg.setColor(color); // Asignamos el color
+        gg.fillOval(x, y, width, height); // Dibujamos la pelota
     }
     public void update(){ // Metodo update para actualizar
-        x += velX; // Actualizamos la posicion en X
-        y += velY; // Actualizamos la posicion en Y
-        if(x < 0 || x > 300 - width) // Si la posicion en X es menor que 0 o mayor que 300 menos el ancho
+        x += velX; // Sumamos la velocidad en X a la posicion en X
+        y += velY; // Sumamos la velocidad en Y a la posicion en Y
+        if(x < 0 || x > 400 - width){ // Si la posicion en X es menor que 0 o mayor que 300 menos el ancho
             velX = -velX; // Cambiamos el sentido de la velocidad en X
-        if(y < 0 || y > 300 - height) // Si la posicion en Y es menor que 0 o mayor que 300 menos el alto
+            x += velX; // Sumamos la velocidad en X a la posicion en X
+        }
+        if(y < 0 || y > 400 - height){ // Si la posicion en Y es menor que 0 o mayor que 300 menos el alto
             velY = -velY; // Cambiamos el sentido de la velocidad en Y
-    }
-    public void run(){
-        do{ // Bucle do while
-            update(); // Llamamos al metodo update
-            try { // Manejo de excepciones
-                Thread.sleep(100); // Tiempo de espera
-            } catch (InterruptedException ex) {} // Excepcion a manejar
-        }while(true); // Bucle infinito
+            y += velY; // Sumamos la velocidad en Y a la posicion en Y
+        }
     }
 }

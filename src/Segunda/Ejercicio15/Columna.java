431b8e12e9dbc76278f7e2bdf050d72d71f102bf
Programador753 <antoniohcavero@outlook.com>
2025-02-12 09:03:33 +0100

    actualizo

--- src/Segunda/Ejercicio15/Columna.java
+++ src/Segunda/Ejercicio15/Columna.java
@@ -1,40 +1,30 @@
 package Segunda.Ejercicio15;
 
-import java.awt.Color; // Importo clase Graphics
-import java.awt.Graphics; // Importo clase Color
-import java.awt.Rectangle; // Importo clase Graphics
+import java.awt.Color; // Importo clase Rectangle de la libreria awt
+import java.awt.Graphics; // Importo clase Color de la libreria awt
+import java.awt.Rectangle; // Importo clase Graphics de la libreria awt
 
 public class Columna{ // clase Columna formada por 2 rectangulos separados por un espacio entre medios para formar la columna
-    // Un objeto de la clase Columna tiene 2 rectangulos y un espacio entre ellos
-    Rectangle arriba; // Creo un objeto de la clase Rectangle con una longitud entre 20 y 125
-    Rectangle abajo; // Creo un objeto de la clase Rectangle para la parte inferior de la columna
-    public static final int ESPACIO = 100; // Creo un entero espacio
-    public static final int ANCHO = 25; // Ancho de la columna
-    
+    public static final int HUECO = 80; // Constante de la clase Columna
+    public static final int ANCHURA = 30; // Constante de la clase Columna 
+    public static final int POSINICIAL = 300; // Constante de la clase Columna 
+    public static final int VELX = -1; // Constante de la clase Columna 
+    Rectangle rect1, rect2; // Declaro 2 objetos de la clase Rectangle para formar la columna
 
-    public Columna(int x, int y, int alto){ // Constructor de la clase Columna
-        arriba = new Rectangle(x, y, ANCHO, alto ); // Creo un objeto de la clase Rectangle
-        abajo = new Rectangle(x, y + alto + 50, ANCHO, 300 - alto - 50); // Creo un objeto de la clase Rectangle
+    public Columna(){ // Constructor de la clase Columna
+        int altura = (int)(Math.random() * 170) + 20; // Genero un numero aleatorio entre 20 y 150 para la altura de la columna
+        rect1 = new Rectangle(POSINICIAL, 0, ANCHURA, altura); // Creo el primer rectangulo de la columna con la altura generada
+        rect2 = new Rectangle(POSINICIAL, altura + HUECO, ANCHURA, 300 - (altura + HUECO)); // Creo el segundo rectangulo de la columna con la altura generada y el hueco
+
     }
-
-    public void mover(){ // Metodo mover de la clase Columna
-        arriba.x -= 1; // Decremento la coordenada x del objeto arriba
-        abajo.x -= 1; // Decremento la coordenada x del objeto abajo
-    }
-
     public void paint(Graphics g){ // Metodo paint de la clase Columna
-        g.setColor(Color.green); // Establezco el color del objeto g
-        g.fillRect(arriba.x, arriba.y, arriba.width, arriba.height); // Dibujo un rectangulo con las coordenadas y dimensiones del objeto arriba
-        g.fillRect(abajo.x, abajo.y, abajo.width, abajo.height); // Dibujo un rectangulo con las coordenadas y dimensiones del objeto abajo
+        g.setColor(Color.RED); // Establezco el color verde
+        g.fillRect(rect1.x, rect1.y, rect1.width, rect1.height); // Dibujo el primer rectangulo de la columna
+        g.fillRect(rect2.x, rect2.y, rect2.width, rect2.height); // Dibujo el segundo rectangulo de la columna
+        
     }
-
-    public boolean colision(Rectangle pajaro){ // Metodo colision de la clase Columna
-        if(arriba.intersects(pajaro) || abajo.intersects(pajaro)){ // Si el objeto arriba o abajo intersecan con el objeto pajar
-            return true;
-        }
-        return false; // Retorno falso
+    public void update(){ // Metodo update de la clase Columna
+        rect1.x += VELX; // Muevo el primer rectangulo de la columna
+        rect2.x += VELX; // Muevo el segundo rectangulo de la columna
     }
-
-
-
 }
--- src/Segunda/Ejercicio15/FlappyBird.java
+++ src/Segunda/Ejercicio15/FlappyBird.java
@@ -5,41 +5,39 @@ import java.awt.Color; // Importo clase Graphics
 import java.awt.Event; // Importo clase Image
 import java.awt.Graphics; // Importo clase Color
 import java.awt.Image; // Importo clase Event
+import java.util.ArrayList; // Importo clase ArrayList
 
 
 public class FlappyBird extends Applet implements Runnable {
-    
+    public static final int TIEMPO = 35; // Creo una constante de tiempo
     Thread animacion; // Creo un hilo de ejecucion
     Image imagen; // Creo una imagen
     Graphics noseve; // Creo un objeto de la clase Graphics
     Pajaro pajaro; // Creo un objeto de la clase Pajaro
-    Columna[] columnas; // Creo un array de objetos de la clase Columna
+    ArrayList<Columna> columnas; // Creo un objeto de la clase Columna
+    boolean continua = true; // Creo una variable booleana
+    int contador = 0; // Creo una variable contador
+    
     
     @Override
     public void init() { // Metodo init para inicializar
-        imagen = createImage(300, 300); // Creo una imagen de 300x300
+        imagen = this.createImage(300, 300); // Creo una imagen de 300x300
         noseve = imagen.getGraphics(); // Asigno la imagen a noseve
+        pajaro = new Pajaro(); // Creo un objeto de la clase Pajaro
+        columnas = new ArrayList<Columna>(); // Creo un objeto de la clase Columna
+        columnas.add(new Columna()); // Añado una nueva columna
         this.setSize(300, 300); // Asigno tamaño al applet
-        pajaro = new Pajaro(10, 0, 15, 15); // Creo un objeto de la clase Pajaro
-        columnas = new Columna[300 / (Columna.ANCHO + 50)]; // Creo un array de objetos de la clase Columna
-        for (int i = 0; i < columnas.length; i++) { // Bucle para recorrer el array de columnas
-            columnas[i] = new Columna(300 + i * (Columna.ANCHO + 50), 0, (int) (Math.random() * 200) + 50); // Creo un objeto de la clase Columna con separación adicional
-        }
+        
+    }
+    @Override
+    public void update (Graphics g) { // Metodo update para actualizar
+        paint(g); // Llamo al metodo paint
     }
 
-    public void animacion() { // Metodo animacion para actualizar el estado del juego
-        pajaro.actualizar(); // Actualizo el estado del pajaro
-        if (pajaro.y > 300) // Si la posicion y del pajaro es mayor que 300
-            pajaro.y = 0; // La posicion y del pajaro es 0
-        for (Columna columna : columnas) { // Bucle para recorrer el array de columnas
-            columna.mover(); // Muevo las columnas
-            if (columna.colision(pajaro)) { // Si hay colision entre las columnas y el pajaro
-                pajaro.y = 0; // La posicion y del pajaro es 0
-                for (int j = 0; j < columnas.length; j++) { // Bucle para recorrer el array de columnas
-                    columnas[j] = new Columna(300 + j * (Columna.ANCHO + 50), 0, (int) (Math.random() * 200) + 50); // Creo un objeto de la clase Columna con separación adicional
-                }
-            }
-        }
+    @Override
+    public void start() { // Metodo start para iniciar el hilo
+        animacion = new Thread(this); // Creo un hilo de ejecucion
+        animacion.start(); // Inicio el hilo
     }
 
     @Override
@@ -47,35 +45,50 @@ public class FlappyBird extends Applet implements Runnable {
         noseve.setColor(Color.black); // Asigno color negro
         noseve.fillRect(0, 0, 300, 300); // Relleno el fondo de negro
         pajaro.paint(noseve); // Llamo al metodo paint de la clase Pajaro
-        for (Columna columna : columnas) { // Bucle para recorrer las columnas
-            columna.paint(noseve); // Pinto las columnas
-        }
-        g.drawImage(imagen, 0, 0, this); // Dibujo la imagen
-    }
-
-    @Override
-    public void start() { // Metodo start para iniciar el hilo
-        if (animacion == null) { // Si el hilo es nulo
-            animacion = new Thread(this); // Creo un nuevo hilo
-            animacion.start(); // Inicio el hilo
-        }
+        for(Columna columna : columnas) // Foreach para recorrer la lista
+            columna.paint(noseve); // Llamo al metodo paint de la clase columna 
+        if(!continua) noseve.drawString("GAME OVER", 120,140); // Compruebo si continua
+            g.drawImage(imagen, 0, 0, this); // Dibujo la imagen
     }
 
     @Override
     public boolean keyDown(Event e, int tecla) { // Metodo keyDown para mover el pajaro
-        if (tecla == Event.UP) // Si la tecla es la flecha arriba
-            pajaro.saltar();
-        return true;
+        if(tecla == 32) // Si pulso la tecla espacio
+            pajaro.saltar();  // LLamo a saltar 
+        return true; // retorno verdadero 
     }
 
     @Override
     public void run() { // Metodo run para ejecutar el hilo
         do {
-            animacion(); // Llamo al metodo animacion
-            repaint(); // Llamo al metodo repaint
+            contador += TIEMPO; // Incremento contador 
+            if(contador > 3000){ // Compruebo vlaor contador
+                columnas.add(new Columna()); // Añado nueva columna 
+                contador = 0; // Reinicio contador 
+            }
+            pajaro.update(); // LLamo a actualizar de la clase pajaro
+
+            for(Columna columna : columnas) // Foreach para recorrer la lista
+                columna.update(); // LLamo a actualizar 
+
+            if(!columnas.isEmpty()) // Compruebo si la columna esta vacia
+                if(columnas.get(0).rect1.x < -Columna.ANCHURA) // Compruebo si la columna esta fuera de la pantalla
+                    columnas.remove(0); // Elimino la columna
+            
+            continua = !pajaro.colision(columnas.get(0)); // Continua es falso
+            
+            if(pajaro.y > 300 || pajaro.y < 0) // Compruebo si el pajaro esta fuera de la antalla
+                continua = false; // Continua es falsop
+            
+            if(!continua){ // Si continua es falso
+                repaint(); // Repinto
+                animacion.stop(); // Paro el hilo de ejecucion 
+            }
+
+            repaint(); // repinto 
             try {
-                Thread.sleep(20); // Hago una pausa de 20 milisegundos
-            } catch (InterruptedException e) {}
+                Thread.sleep(TIEMPO); // Hago una pausa de 20 milisegundos
+            } catch (InterruptedException e) {} // Capturo excepcion 
         } while (true); // Bucle infinito
     }
 }

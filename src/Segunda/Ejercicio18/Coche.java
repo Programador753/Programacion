7bba911f1c2024acb7a8da890a59fbdebf6a85bb
Programador753 <antoniohcavero@outlook.com>
2025-02-21 13:58:48 +0100

    actualizo

---
+++ src/Segunda/Ejercicio18/Coche.java
@@ -0,0 +1,14 @@
+package Segunda.Ejercicio18;
+
+import java.awt.Color; // Importo clase color
+import java.awt.Rectangle; // Importo clase rectangulo
+
+
+
+public class Coche extends Rectangle{
+    static final Color colores[] = {Color.red, Color.blue, Color.green, Color.black, Color.orange, Color.MAGENTA, Color.pink, Color.cyan}; // Creo un array de colores
+    Color color;
+    int velX; 
+    
+    
+}
--- src/Segunda/Ejercicio18/Rana.java
+++ src/Segunda/Ejercicio18/Rana.java
@@ -1,5 +1,39 @@
 package Segunda.Ejercicio18;
 
-public class Rana {
-    
+import java.awt.Color; // Importamos la clase Rectangle
+import java.awt.Graphics; // Importamos la clase Color
+import java.awt.Rectangle; // Importamos la clase Graphics
+
+
+public class Rana extends Rectangle {
+    public static final int VEL = 5; // Velocidad vertical de la rana (constante)
+    public static final int ARRIBA = 1004; // Constante para la dirección arriba con la flecha arriba
+    public static final int ABAJO = 1005; // Constante para la dirección abajo
+    public static final int IZQUIERDA = 1006; // Constante para la dirección izquierda
+    public static final int DERECHA = 1007; // Constante para la dirección derecha
+
+    public Rana(){
+        super(245, 260, 30, 40); // Inicializamos la rana en la posición (245, 260) con un tamaño de 30x40 con la llamada a la superclase.
+
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.GREEN); // Establecemos el color de la rana a verde
+        g.fillRect(x, y, width, height); // Dibujamos la rana como un rectángulo
+    }
+    public void actualizar(int dirección){
+        switch (dirección) {
+            case ARRIBA:
+                y -= VEL; // Mueve la rana hacia arriba
+                break;
+            case ABAJO:
+                y += VEL; // Mueve la rana hacia abajo
+                break;
+            case IZQUIERDA:
+                x -= VEL; // Mueve la rana hacia la izquierda
+                break;
+            case DERECHA:
+                x += VEL; // Mueve la rana hacia la derecha
+                break;
+        }
+    }
 }

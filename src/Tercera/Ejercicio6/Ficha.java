9591d8836d91ddeb94b2eb74f4303917e79b52ab
Programador753 <antoniohcavero@outlook.com>
2025-04-09 11:02:17 +0200

    actualizo

--- src/Tercera/Ejercicio6/Ficha.java
+++ src/Tercera/Ejercicio6/Ficha.java
@@ -11,9 +11,11 @@ public class Ficha extends Rectangle {
 
     public static final int DIM = 76;
     public Image fichaImage;
-    public int precio;
+    private int precio;
     
     ArrayList<Integer> lCasillas; // Lista para almacenar los números de las casillas
+    private ArrayList<Integer> numerosApostados; // Lista para almacenar los números apostados
+    
 
     // Constructor de la clase Ficha
     public Ficha(int x, int y, int v, Image fichaImage) {
@@ -21,6 +23,7 @@ public class Ficha extends Rectangle {
         this.fichaImage = fichaImage;
         this.precio = v;
         lCasillas = new ArrayList<Integer>(); // Inicializa la lista de casillas
+        numerosApostados = new ArrayList<Integer>(); // Inicializa la lista de números apostados
     }
 
     public void paint(Graphics g, Applet applet) {
@@ -38,9 +41,32 @@ public class Ficha extends Rectangle {
         for (int i = 0; i < casillas.length; i++) {
             for (int j = 0; j < casillas[i].length; j++) {
                 if (casillas[i][j].intersects(this)) {
-                    lCasillas.add(casillas[i][j].valor); // Añade el número de la casilla a la lista
+                    lCasillas.add(casillas[i][j].valor); // Añade el número de la casilla a la lista de casillas
+                    // Si la casilla ya está en la lista de números apostados, no la añade de nuevo
+                    if (!numerosApostados.contains(casillas[i][j].valor)) {
+                        numerosApostados.add(casillas[i][j].valor); // Añade el número de la casilla a la lista de números apostados
+                    }
                 }
             }
         }
     }
+    
+    public ArrayList<Integer> getNumerosApostados() {
+        return numerosApostados;
+    }
+
+    public void setNumerosApostados(ArrayList<Integer> numerosApostados) {
+        this.numerosApostados = numerosApostados;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    
+    
 }
